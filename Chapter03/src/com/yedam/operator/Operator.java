bc06353a30ad66c9e575108dc9a1130bbbb7a26a
admin <admin@YD05-19>
2023-01-27 17:59:40 +0900

    Chapter03

--- Chapter03/src/com/yedam/operator/Operator.java
+++ Chapter03/src/com/yedam/operator/Operator.java
@@ -3,8 +3,12 @@ package com.yedam.operator;
 public class Operator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//증감연산자
+		int a = 0;
+		System.out.println(a++);
+		
+		System.out.println(++a);
+		
 	}
 
 }
