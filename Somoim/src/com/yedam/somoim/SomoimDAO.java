d0a8e649e7dde9b27807155d9757dfc8f08c1de1
tnsej <tnsej@LAPTOP-JASVGN56>
2023-02-22 08:20:00 +0900

    homework

--- Somoim/src/com/yedam/somoim/SomoimDAO.java
+++ Somoim/src/com/yedam/somoim/SomoimDAO.java
@@ -1,5 +1,8 @@
 package com.yedam.somoim;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.yedam.common.DAO;
 
 public class SomoimDAO extends DAO{
@@ -50,30 +53,148 @@ public class SomoimDAO extends DAO{
 	
 	
 	//새 회원 등록
-	
+	public int insertMember(Somoim somo) {
+		int result = 0;
+		
+		try {
+			conn();
+			
+			String sql = "INSERT INTO somoim \n"
+					+ "VALUES(?,?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, somo.getMemberId());
+			pstmt.setString(2, somo.getMemberName());
+			pstmt.setDate(3, somo.getJoinDate());
+			pstmt.setString(4, somo.getEmail());
+			pstmt.setInt(5, somo.getMemberAge());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		return result;
+	}
 	
 	
 	
 	//모든 회원 조회 -> 가입 날자 순서대로 조회
-	
-	
+	public List<Somoim> getMemberList() {
+		List<Somoim> list = new ArrayList<>();
+		
+		Somoim somo = null;
+		
+		try {
+			conn();
+			String sql = "SELECT * \n"
+					+ "FROM somoim\n"
+					+ "ORDER BY join_date";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				somo = new Somoim();
+				somo.setMemberId(rs.getString("member_id"));
+				somo.setMemberName(rs.getString("memeber_name"));
+				somo.setJoinDate(rs.getDate("join_date"));
+				somo.setMemberAge(rs.getInt("member_age"));
+				
+				list.add(somo);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		return list;
+		
+	}
 	
 	
 	//아이디로 회원 조회
-	
-	
+	public Somoim getMember() {
+		Somoim somo = null;
+		
+		try {
+			conn();
+			String sql = "SELECT * \n"
+					+ "FROM somoim\n"
+					+ "WHERE member_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, SomoimService.somoimInfo.getMemberId());
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				somo = new Somoim();
+				somo.setMemberId(rs.getString("member_id"));
+				somo.setMemberName(rs.getString("memeber_name"));
+				somo.setJoinDate(rs.getDate("join_date"));
+				somo.setMemberAge(rs.getInt("member_age"));
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		return somo;
+	}
 	
 	
 	
 	//이메일 변경
-	
-	
+	public int updateEmail(Somoim somo) {
+		int result =0;
+		
+		try {
+			conn();
+			
+			String sql = "UPDATE somoim\n"
+					+ "SET email = ?"
+					+ "WHERE member_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, somo.getMemberId());
+			pstmt.setString(2, somo.getEmail());
+			
+			result = pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		
+		return result;
+	}
 	
 	
 	//소모임 인원 강제 탈퇴
+	public int deleteMember(String memberId) {
+		int result = 0;
+		
+		try {
+			conn();
+			
+			String sql = "DELETE FROM somoim\n"
+					+ "WHERE member_id =?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			
+			result = pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconn();
+		}
+		
+		return result;
+	}
 	
 	
-	
-	
+
 	
 }
--- Somoim/src/com/yedam/somoim/SomoimService.java
+++ Somoim/src/com/yedam/somoim/SomoimService.java
@@ -1,5 +1,83 @@
 package com.yedam.somoim;
 
-public class SomoimService {
+import java.util.Scanner;
 
+public class SomoimService {
+	//member의 정보를 자바 전역에 공유하기 위해서 static
+	//1. memberInfo -> null이 아니다. null이다.
+	//null -> O 로그인이 안된 상태
+	//null -> x 로그인이 되어 있는 상태
+	
+	public static Somoim somoimInfo = null;
+	Scanner sc = new Scanner(System.in);
+	
+	//로그인
+	public void login() {
+		Somoim somo = null;
+		System.out.println("ID>");
+		String memberID = sc.nextLine();
+		
+		somo = SomoimDAO.getInstance().login(memberID);
+		
+		if(somo != null) {
+			System.out.println("정상 로그인 되었습니다.");
+			System.out.println(somo.getMemberId() + "님 환영합니다!🎉🎉");
+		}else {
+			System.out.println("관리자 아이디가 아닙니다.");
+		}
+		
+	}
+	
+	//새 회원 등록
+	public void insertMember() {
+		System.out.println("회원 ID>");
+		int result = SomoimDAO.getInstance().insertMember(sc.nextLine());
+		
+		if(result > 0) {
+			System.out.println("회원 ID 등록 완료");
+		}else {
+			System.out.println("회원 ID 등록 실패");
+		}
+		
+	}
+	
+	//모든 회원 조회 -> 가입 날자 순서대로 조회
+	
+	//아이디로 회원 조회
+	public void getMember() {
+		Somoim somo = SomoimDAO.getInstance().getMember();
+		System.out.println(somo.toString());
+	}
+	
+	//이메일 변경
+	public void updateEamil() {
+		Somoim somo = new Somoim();
+		System.out.println("회원 ID>");
+		somo.setMemberId(sc.nextLine());
+		System.out.println("Email 변경사항>");
+		somo.setEmail(sc.nextLine());
+		
+		int result= SomoimDAO.getInstance().updateEmail(somo);
+		
+		if(result > 0) {
+			System.out.println("Email 수정 완료");
+		}else {
+			System.out.println("Email 수정 실패");
+		}
+		
+	}
+	
+	//소모임 인원 강제 탈퇴
+	public void deleteMember() {
+		System.out.println("회원 ID>");
+		int result = SomoimDAO.getInstance().deleteMember(sc.nextLine());
+		
+		if(result > 0) {
+			System.out.println("회원 강제 탈퇴 완료");
+		}else {
+			System.out.println("회원 강제 탈퇴 완료");
+		}
+		
+	}
+	
 }
